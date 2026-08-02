e71049251a864b2108f78dbb2fdad8e7a1f39035
nirtisum-cs <nirtisum.cs@gmail.com>
2026-08-03 04:37:45 +0600

    Update CSS and make Entry Component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 export default function Header()
 {
     return <header>
-        <img id = 'img1' src="/public/globe3.png" alt="mini globe" />
+        <img id = 'img1' src="/public/globe.png" alt="mini globe" />
         <h1>my travel journal</h1>
     </header>
 }
