2b4ada1030f4b1e8e72e0165d831d4355aacfd1d
nirtisum-cs <nirtisum.cs@gmail.com>
2026-08-07 03:05:06 +0600

    Add class Names

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,12 +1,18 @@
 export default function Entry()
 {
     return (
-        <article>
-            <img src="/public/mount_fuji.jpg" alt="mount_fuji" />
+        <article className="journalEntry">
+            
+            <div className="main-img-container">
+                <img src="/public/mount_fuji.jpg" alt="mount_fuji" className="mainImg"/>
+            </div>
+            
+
             <div>
                 <img src="/public/marker.png" alt="marker" />
                 <span>Japan</span>
                 <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View On Google Maps</a>
+                <h2>Mount Fuji</h2>
             </div>
         </article>
     )
