8c170f750f413633f48696d4a6de42b531662311
nirtisum-cs <nirtisum.cs@gmail.com>
2026-08-06 23:55:32 +0600

    Add Entry

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Header from './components/header.jsx'
-
+import Entry from './components/Entry.jsx'
 createRoot(document.getElementById('root')).render(
+  <>
   <Header></Header>
+  <Entry />
+  </>
 )
